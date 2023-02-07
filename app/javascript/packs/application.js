434541f897f1b88deddff042d5d5d9aca0a15255
00komiya <114364060+00komiya@users.noreply.github.com>
2023-02-07 09:12:55 +0000

    [update]レビュー機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
 
 Rails.start()
-// Turbolinks.start()
+Turbolinks.start()
 ActiveStorage.start()
 
 import Raty from "raty.js"
